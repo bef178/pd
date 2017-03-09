e74841e0d65b5663b3c8e513292eae51c422afc3
tanghao <tanghao.hb@gmail.com>
2017-03-10 00:09:58 +0800

    java: merge io.Factory into json.Factory

--- src-java/cc/typedef/json/Factory.java
+++ src-java/cc/typedef/json/Factory.java
@@ -7,9 +7,12 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map.Entry;
 
+import cc.typedef.basic.Blob;
+import cc.typedef.io.Codec;
 import cc.typedef.io.InstallmentByteBuffer;
-import cc.typedef.io.ParsingException;
 import cc.typedef.io.InstallmentByteBuffer.Reader;
+import cc.typedef.io.Nextable;
+import cc.typedef.io.ParsingException;
 import cc.typedef.primitive.Ctype;
 
 public class Factory {
@@ -137,13 +140,75 @@ public class Factory {
             }
         }
 
-        private static JsonScalar buildS(final Reader r, Producer producer) {
+        private static JsonScalar buildS(Nextable it, Producer producer) {
             return producer.produceJsonScalar()
-                    .set(cc.typedef.io.Factory.parseString(r).toString());
+                    .set(parseString(it).toString());
+        }
+
+        /**
+         * accept a quoted string
+         */
+        public static CharSequence parseString(Nextable it) {
+            return parseString(it, '\"', '\"');
+        }
+
+        public static CharSequence parseString(Nextable it, final int start,
+                final int end) {
+            int ch = -1;
+            if (start >= 0) {
+                // there is a start delimiter, taste it
+                ch = it.next();
+                if (ch != start) {
+                    throw new ParsingException(start, ch);
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            while (true) {
+                ch = it.next();
+                if (ch == end) {
+                    // consume the end delimiter then exit
+                    return sb;
+                }
+
+                if (ch == '\\') {
+                    ch = Codec.Glyph.decode(ch, it);
+                }
+                sb.appendCodePoint(ch);
+            }
         }
     }
 
     private static class Serializer {
+
+        public static void serialize(CharSequence s, InstallmentByteBuffer w) {
+            w.append('\"');
+            for (int i = 0; i < s.length(); ++i) {
+                char c = s.charAt(i);
+                if (Ctype.isAlphanum(c)) {
+                    w.append(c);
+                    continue;
+                }
+
+                int ch = c;
+                if (Character.isHighSurrogate(c)) {
+                    char chLow = s.charAt(++i);
+                    if (Character.isLowSurrogate(chLow)) {
+                        ch = Character.toCodePoint(c, chLow);
+                    } else {
+                        throw new ParsingException();
+                    }
+                } else if (Character.isLowSurrogate(c)) {
+                    throw new ParsingException();
+                }
+                // faster than String.getBytes("UTF-8") with exception handled
+                // faster than toUtf8() then toHexText()
+                Blob blob = new Blob();
+                Codec.Glyph.encode(ch, blob);
+                w.append(blob.a);
+            }
+            w.append('\"');
+        }
 
         private static InstallmentByteBuffer serialize(Json json,
                 Config config, InstallmentByteBuffer o) {
@@ -230,7 +295,7 @@ public class Factory {
         }
 
         private static void serializeS(String s, InstallmentByteBuffer o) {
-           cc.typedef.io.Factory.serialize(s, o);
+            serialize(s, o);
         }
     }
 
