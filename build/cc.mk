05721dec6e1c1f83aadc08603fafc842d4f78f2c
tanghao <tanghao.hb@gmail.com>
2017-09-02 11:49:08 +0800

    cc: intend to provide additional compiler features within all-in-one header

--- build/cc.mk
+++ build/cc.mk
@@ -2,7 +2,7 @@
 # makefile for cc
 
 CC := gcc
-CCFLAGS = -std=c99 -Werror $(addprefix -include ,$(HEADERS))
+CCFLAGS = -std=c99 -Werror $(addprefix -include ,$(STD_HEADER) $(HEADERS))
 
 PACKAGE := libcliff
 
@@ -11,11 +11,14 @@ SRC := $(TOP)/src/$(subst .,/,$(PACKAGE))
 
 OUT := $(TOP)/out
 OUT_CC := $(OUT)/cc
+OUT_TARGET_H := $(OUT)/$(PACKAGE).h
 OUT_TARGET_A := $(OUT)/$(PACKAGE).a
 OUT_TARGET_O := $(OUT)/$(PACKAGE).o
-OUT_TARGET := $(OUT_TARGET_A) $(OUT_TARGET_O)
+OUT_TARGET := $(OUT_TARGET_H) $(OUT_TARGET_A) $(OUT_TARGET_O)
 
-HEADERS := $(SRC)/libcliff.h
+STD_HEADER := $(SRC)/std.h
+
+HEADERS := $(SRC)/predefined.h $(SRC)/libcliff.h
 OBJECTS :=
 
 #-----------------------------------------------------------
@@ -31,8 +34,8 @@ OBJECTS += $(patsubst $(SRC)/%.c,$(OUT_CC)/%.o,$(shell find -L $(LOCAL_SRC_DIR) 
 
 LOCAL_SRC_DIR := $(SRC)/adt
 
-#HEADERS += $(shell find -L $(LOCAL_SRC_DIR) -name "*.h")
-HEADERS += $(addprefix $(LOCAL_SRC_DIR)/,Blob.h KeyValue.h linear/ListHead.h linear/List.h linear/Deque.h HashMap.h)
+HEADERS += $(shell find -L $(LOCAL_SRC_DIR) -name "*.h")
+#HEADERS += $(addprefix $(LOCAL_SRC_DIR)/,Blob.h KeyValue.h linear/ListHead.h linear/List.h linear/Deque.h HashMap.h)
 OBJECTS += $(patsubst $(SRC)/%.c,$(OUT_CC)/%.o,$(shell find -L $(LOCAL_SRC_DIR) -name "*.c"))
 
 #-----------------------------------------------------------
@@ -41,6 +44,11 @@ OBJECTS := $(strip $(OBJECTS))
 
 .PHONY: cc
 cc: $(OUT_TARGET)
+
+$(OUT_TARGET_H): $(HEADERS)
+	@echo "generating .h ..."
+	@mkdir -p $(OUT_CC)
+	@cat $(HEADERS) > $@
 
 $(OUT_TARGET_A): $(OBJECTS)
 	@echo "archiving ..."
