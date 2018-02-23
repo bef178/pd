d4decd0edede17b48176dc3d99d2831c8596a860
tanghao <tanghao.hb@gmail.com>
2018-02-23 19:33:48 +0800

    fix Base64

--- src-java/libcliff/io/codec/Base64.java
+++ src-java/libcliff/io/codec/Base64.java
@@ -12,38 +12,18 @@ import libcliff.io.PushablePipe;
  */
 public class Base64 implements PullablePipe, PushablePipe {
 
-    private class Puller implements PullablePipe {
+    private static class Puller implements PullablePipe {
 
-        private int[] base64 = null;
+        private int[] src = new int[3];
 
-        private int[] parsed = new int[3];
+        private int srcIndex = 3;
 
-        private int pIndex = 3;
-
-        private boolean ends = false;
+        private boolean eof = false;
 
         /**
          * accept byte pullable
          */
         private Pullable upstream = null;
-
-        private void fromBase64Bytes(int[] base64, int[] dst) {
-            dst[0] = (base64[0] << 2) | (base64[1] >> 4);
-            dst[1] = (base64[1] << 4) | (base64[2] >> 2);
-            dst[2] = (base64[2] << 6) | base64[3];
-        }
-
-        private int getBase64Bytes(Pullable pullable, int[] base64) {
-            assert base64 != null && base64.length >= 4;
-            for (int i = 0; i < 4; ++i) {
-                int aByte = CheckedByte.checkByteEx(pullable.pull());
-                if (aByte == -1) {
-                    return i;
-                }
-                base64[i] = DECODE_MAP[aByte];
-            }
-            return 4;
-        }
 
         @Override
         public Puller join(Pullable upstream) {
@@ -53,44 +33,30 @@ public class Base64 implements PullablePipe, PushablePipe {
 
         @Override
         public int pull() {
-            if (ends) {
+            if (eof) {
                 return -1;
             }
-            if (base64 == null) {
-                // the first time
-                base64 = new int[4];
-                getBase64Bytes(upstream, base64);
+            if (srcIndex == 3) {
+                fromBase64Bytes(src, upstream);
+                srcIndex = 0;
             }
-            if (pIndex == 3) {
-                fromBase64Bytes(base64, parsed);
-                getBase64Bytes(upstream, base64);
-                if (base64[0] == '=') {
-                    parsed[0] = -1;
-                    if (base64[1] == '=') {
-                        parsed[1] = -1;
-                    }
-                }
-                pIndex = 0;
+            int ch = src[srcIndex++];
+            if (ch == -1) {
+                eof = true;
             }
-            int aByte = parsed[pIndex++];
-            if (aByte == -1) {
-                ends = true;
-            }
-            return aByte;
+            return ch;
         }
     }
 
-    private class Pusher implements PushablePipe {
+    private static class Pusher implements PushablePipe {
 
-        private int[] parsed = new int[3];
+        private int[] src = new int[3];
 
-        private int pIndex = 3;
+        private int srcIndex = 0;
 
         private Pushable downstream = null;
 
-        public int flush() {
-            return toBase64Bytes(parsed, 0, pIndex, downstream);
-        }
+        private boolean eof = false;
 
         @Override
         public Pusher join(Pushable downstream) {
@@ -101,53 +67,21 @@ public class Base64 implements PullablePipe, PushablePipe {
         @Override
         public int push(final int ch) {
             CheckedByte.checkByteEx(ch);
-            if (ch == P_FLUSH) {
-                return flush();
+            if (eof) {
+                return E_EOF;
             }
-            parsed[pIndex++] = ch;
-            if (pIndex == 3) {
-                pIndex = 0;
-                return toBase64Bytes(parsed, 0, 3, downstream);
+            if (ch == P_FLUSH) {
+                eof = true;
+                int j = srcIndex;
+                srcIndex = 0;
+                return toBase64Bytes(src, 0, j, downstream);
+            }
+            src[srcIndex++] = ch;
+            if (srcIndex == 3) {
+                srcIndex = 0;
+                return toBase64Bytes(src, 0, 3, downstream);
             }
             return 0;
-        }
-
-        private int toBase64Bytes(int byte0, int byte1, int byte2,
-                Pushable pushable) {
-            int size = 0;
-            size += pushable.push(
-                    ENCODE_MAP[(byte0 & 0xFF) >>> 2]);
-            size += pushable.push(
-                    ENCODE_MAP[((byte0 & 0x03) << 4) | ((byte1 & 0xFF) >>> 4)]);
-            size += pushable.push(
-                    ENCODE_MAP[((byte1 & 0x0F) << 2) | ((byte2 & 0xFF) >>> 6)]);
-            size += pushable.push(
-                    ENCODE_MAP[byte2 & 0x3F]);
-            return size;
-        }
-
-        private int toBase64Bytes(int[] a, int i, int j, Pushable pushable) {
-            switch (j - i) {
-                case 0:
-                    return 0;
-                case 1: {
-                    int size = 0;
-                    size += toBase64Bytes(a[i], 0, 0, pushable);
-                    size += pushable.push('=');
-                    size += pushable.push('=');
-                    return size;
-                }
-                case 2: {
-                    int size = 0;
-                    size += toBase64Bytes(a[i], a[i + 1], 0, pushable);
-                    size += pushable.push('=');
-                    return size;
-                }
-                case 3:
-                    return toBase64Bytes(a[i], a[i + 1], a[i + 2], pushable);
-                default:
-                    throw new ParsingException();
-            }
         }
     }
 
@@ -167,7 +101,7 @@ public class Base64 implements PullablePipe, PushablePipe {
         };
 
         DECODE_MAP = new int[127];
-        Arrays.fill(DECODE_MAP, (char) -1);
+        Arrays.fill(DECODE_MAP, -1);
         for (int i = 0; i < ENCODE_MAP.length; ++i) {
             DECODE_MAP[ENCODE_MAP[i]] = i;
         }
@@ -175,32 +109,91 @@ public class Base64 implements PullablePipe, PushablePipe {
 
     public static final int P_FLUSH = -1;
 
+    private static void fromBase64Bytes(int[] a, Pullable pullable) {
+        int i = CheckedByte.checkByteEx(pullable.pull());
+        i = i == -1 ? -1 : DECODE_MAP[i];
+        int j = CheckedByte.checkByteEx(pullable.pull());
+        j = j == -1 ? -1 : DECODE_MAP[j];
+        int k = CheckedByte.checkByteEx(pullable.pull());
+        k = k == -1 ? -1 : DECODE_MAP[k]; // '=' => -1
+        int l = CheckedByte.checkByteEx(pullable.pull());
+        l = l == -1 ? -1 : DECODE_MAP[l];
+        a[0] = (i << 2) | (j >> 4);
+        a[1] = k != -1 ? (j << 4) & 0xFF | (k >> 2) : -1;
+        a[2] = l != -1 ? (k << 6) & 0xFF | l : -1;
+    }
+
+    private static int toBase64Bytes(int[] a, int i, int j,
+            Pushable pushable) {
+        switch (j - i) {
+            case 0:
+                return 0;
+            case 1: {
+                pushable.push(ENCODE_MAP[(a[i] & 0xFF) >>> 2]);
+                pushable.push(ENCODE_MAP[(a[i] & 0x03) << 4]);
+                pushable.push('=');
+                pushable.push('=');
+                break;
+            }
+            case 2: {
+                pushable.push(ENCODE_MAP[(a[i] & 0xFF) >>> 2]);
+                pushable.push(ENCODE_MAP[((a[i] & 0x03) << 4)
+                                         | ((a[i + 1] & 0xFF) >>> 4)]);
+                pushable.push(ENCODE_MAP[((a[i + 1] & 0x0F) << 2)]);
+                pushable.push('=');
+                break;
+            }
+            case 3:
+                pushable.push(ENCODE_MAP[(a[i] & 0xFF) >>> 2]);
+                pushable.push(ENCODE_MAP[((a[i] & 0x03) << 4)
+                                         | ((a[i + 1] & 0xFF) >>> 4)]);
+                pushable.push(ENCODE_MAP[((a[i + 1] & 0x0F) << 2)
+                                         | ((a[i + 2] & 0xFF) >>> 6)]);
+                pushable.push(ENCODE_MAP[a[i + 2] & 0x3F]);
+                break;
+            default:
+                throw new ParsingException();
+        }
+        return 4;
+    }
+
     private Puller puller = null;
 
     private Pusher pusher = null;
 
-    public Base64() {
-        puller = new Puller();
-        pusher = new Pusher();
-    }
-
     @Override
-    public Puller join(Pullable upstream) {
+    public PullablePipe join(Pullable upstream) {
+        promisePuller();
         return puller.join(upstream);
     }
 
     @Override
-    public Pusher join(Pushable downstream) {
+    public PushablePipe join(Pushable downstream) {
+        promisePusher();
         return pusher.join(downstream);
+    }
+
+    private void promisePuller() {
+        if (puller == null) {
+            puller = new Puller();
+        }
+    }
+
+    private void promisePusher() {
+        if (pusher == null) {
+            pusher = new Pusher();
+        }
     }
 
     @Override
     public int pull() {
+        promisePuller();
         return puller.pull();
     }
 
     @Override
     public int push(int aByte) {
+        promisePusher();
         return pusher.push(aByte);
     }
 }
