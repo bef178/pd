2e304138ffbf213ccad763b450113eaf88a1125b
tanghao <tanghao.hb@gmail.com>
2018-08-24 19:50:21 +0800

    add state check for IntScanner in case back() called before first next() called

--- src/libjava/io/format/IntScanner.java
+++ src/libjava/io/format/IntScanner.java
@@ -3,19 +3,17 @@ package libjava.io.format;
 import libjava.io.Pullable;
 
 /**
- * Often, a parser requires at least one symbol to speculate which token could
- * be expected. The underlying scanner must takes both the symbol and the
- * rest of stream to produce a legal token. To merge those two, the parser need
- * last() or peek() or back().<br/>
+ * Often, a parser requires at least one symbol to speculate which token
+ * could be expected. The underlying scanner must takes both the symbol and
+ * the rest of stream to produce a legal token. To merge those two, the
+ * stream ought to provide last(), peek() or back().<br/>
  * <br/>
- * Please don't suppose a stream is physically able to move backward.<br/>
- * &emsp;- if using last(), user cannot get all symbols by continuously
- * calling pull(), and hasLast() must be added.<br/>
- * &emsp;- if using peek(), every time peek() following pull(), the stream
- * indeed moves one step - one symbol will be lost if the stream being held
- * outside calls pull().<br/>
- * &emsp;- if using back(), user should be aware only one backward step could
- * succeed - it is also the C way.<br/>
+ * These 3 are almost equivalent with a one-slot cache - please don't suppose
+ * a stream being able to go backward physically. The difference is subtle:<br/>
+ * &emsp;- last() and peek() are similar: hide movement and give the value or
+ * throw error state<br/>
+ * &emsp;- back() the C way is just movement: a boolean shows the state
+ * without exception<br/>
  */
 public class IntScanner implements Pullable {
 
@@ -23,26 +21,30 @@ public class IntScanner implements Pullable {
         return new IntScanner(Pullable.wrap(cs));
     }
 
-    private boolean backed;
+    private boolean backed; // uses last
 
     private int last;
 
     private Pullable upstream;
 
-    public IntScanner(int last, Pullable rest) {
+    private int offset;
+
+    public IntScanner(int first, Pullable rest) {
         this.backed = true;
-        this.last = last;
+        this.last = first;
         this.upstream = rest;
+        this.offset = 1;
     }
 
     public IntScanner(Pullable upstream) {
         this.backed = false;
         this.last = 0;
         this.upstream = upstream;
+        this.offset = 0;
     }
 
     public boolean back() {
-        if (backed) {
+        if (backed || offset == 0) {
             return false;
         }
 
@@ -55,6 +57,7 @@ public class IntScanner implements Pullable {
             backed = false;
             return last;
         }
+        ++offset;
         return last = this.upstream.pull();
     }
 
