f54e93c59b0a4663b2a9575ba9ccc38f6bca789b
tanghao <tanghao.hb@gmail.com>
2019-01-31 15:55:46 +0800

    fix Cstring

--- src/pd/cprime/Cstring.java
+++ src/pd/cprime/Cstring.java
@@ -5,6 +5,22 @@ import java.util.List;
 
 public final class Cstring {
 
+    public static final int compare(String s1, String s2) {
+        if (s1 == null) {
+            if (s2 == null) {
+                return 0;
+            } else {
+                return -1;
+            }
+        } else {
+            if (s2 == null) {
+                return 1;
+            } else {
+                return s1.compareTo(s2);
+            }
+        }
+    }
+
     public static boolean equals(String s1, String s2) {
         if (s1 == null) {
             return s2 == null;
@@ -16,36 +32,45 @@ public final class Cstring {
     public static int hashCode(String... a) {
         int hashCode = 0;
         for (String s : a) {
-            hashCode = hashCode * 31 + s == null ? 0 : s.hashCode();
+            hashCode = hashCode * 31 + (s == null ? 0 : s.hashCode());
         }
         return hashCode;
     }
 
     public static String[] split(String s, final int delimeter) {
         List<String> list = new LinkedList<String>();
-        int start = 0;
-        int end = 0;
-        while (end < s.length()) {
-            char c = s.charAt(end);
+        int start = -1;
+        int i = 0;
+        while (i < s.length()) {
+            if (start == -1) {
+                start = i;
+            }
+            char c = s.charAt(i);
             int ch;
+            boolean atHighSurrogate = false;
             if (Character.isHighSurrogate(c)) {
-                ch = s.codePointAt(end);
-                ++end;
+                ch = s.codePointAt(i);
+                atHighSurrogate = true;
             } else if (Character.isLowSurrogate(c)) {
                 throw new IllegalArgumentException();
             } else {
                 ch = c;
             }
-            ++end;
             if (ch == delimeter) {
-                String token = s.substring(start, end);
+                String token = s.substring(start, i);
                 list.add(token);
-                start = end;
+                start = -1;
+            }
+            if (atHighSurrogate) {
+                i += 2;
+            } else {
+                i++;
             }
         }
-        if (s.isEmpty() || start != end) {
-            list.add(s.substring(start, end));
+        if (start == -1) {
+            start = i;
         }
+        list.add(s.substring(start));
         return list.toArray(new String[0]);
     }
 }
