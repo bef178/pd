c129a5f708d3b9a88d59ec7f658d9f7b676e1cb5
tanghao <tanghao.hb@gmail.com>
2019-05-06 20:42:55 +0800

    add Util for calculation

---
+++ src/pd/time/calendar/gregorian/LocalField.java
@@ -0,0 +1,6 @@
+package pd.time.calendar.gregorian;
+
+public enum LocalField {
+    YEAR, DAY_OF_YEAR, MONTH_OF_YEAR, DAY_OF_MONTH, WEEK_OF_YEAR, DAY_OF_WEEK,
+    HOUR, MINUTE, SECOND, MILLISECOND;
+}
