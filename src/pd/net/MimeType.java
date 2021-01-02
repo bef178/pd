1957c5a98faf65471416cb3a1b1a3abdbdb7f5ec
tanghao <tanghao.hb@gmail.com>
2021-01-03 00:28:59 +0800

    add a local db ext2mime to MimeType

--- src/pd/net/MimeType.java
+++ src/pd/net/MimeType.java
@@ -1,9 +1,37 @@
 package pd.net;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * http://en.wikipedia.org/wiki/MIME
  */
 public final class MimeType {
+
+    public static final Map<String, String> ext2mime;
+
+    static {
+        HashMap<String, String> m = new HashMap<>();
+        m.put("bin", "application/octet-stream");
+        m.put("css", "text/css");
+        m.put("gif", "image/gif");
+        m.put("html", "text/html");
+        m.put("ico", "image/x-icon");
+        m.put("jpg", "image/jpeg");
+        m.put("js", "text/javascript");
+        m.put("json", "application/json"); // RFC 4627
+        m.put("pdf", "application/pdf");
+        m.put("png", "image/png");
+        m.put("svg", "image/svg+xml");
+        m.put("swf", "application/x-shockwave-flash");
+        m.put("txt", "text/plain");
+        m.put("wav", "audio/x-wav");
+        m.put("wma", "audio/x-ms-wma");
+        m.put("wmv", "video/x-ms-wmv");
+        m.put("xml", "text/xml");
+        ext2mime = Collections.unmodifiableMap(m);
+    }
 
     public static MimeType fromString(String mimeString) {
         String[] a = mimeString.split("/");
@@ -25,29 +53,16 @@ public final class MimeType {
         this.minor = minor.toLowerCase();
     }
 
-    public String toString() {
-        return major + '/' + minor;
-    }
-
     /**
      * text/* covers text/json, but not vice versa
-     * 
      */
     public boolean covers(MimeType another) {
         return (major.equals("*") || major.equals(another.major))
                 && (minor.equals("*") || minor.equals(another.minor));
     }
 
-    public boolean isVideo() {
-        return major.equals("video");
-    }
-
     public boolean isAudio() {
         return major.equals("audio");
-    }
-
-    public boolean isImage() {
-        return major.equals("image");
     }
 
     /**
@@ -62,5 +77,18 @@ public final class MimeType {
         return mimeString.equals("inode/directory")
                 || mimeString.equals("application/x-directory")
                 || mimeString.equals("vnd.android.document/directory");
+    }
+
+    public boolean isImage() {
+        return major.equals("image");
+    }
+
+    public boolean isVideo() {
+        return major.equals("video");
+    }
+
+    @Override
+    public String toString() {
+        return major + '/' + minor;
     }
 }
