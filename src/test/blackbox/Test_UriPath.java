a6b2a8f925f2ff616bbb1d74b7c8669892981baa
tanghao <tanghao.hb@gmail.com>
2022-09-13 22:55:32 +0800

    remove unnecessary main

--- src/test/blackbox/Test_UriPath.java
+++ src/test/blackbox/Test_UriPath.java
@@ -11,10 +11,6 @@ import pd.util.PathExtension;
 
 public class Test_UriPath {
 
-    // vm arguments: -ea
-    public static void main(String[] args) {
-    }
-
     @Test
     public void test_getBasename() {
         LinkedHashMap<String, String> testcases = new LinkedHashMap<>();
