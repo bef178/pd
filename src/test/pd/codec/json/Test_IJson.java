33c2337da8d70484ef343a104bba2b99f14e2f3d
tanghao <tanghao.hb@gmail.com>
2022-10-04 16:25:56 +0800

    fix: add equals() and hashCode() to SimpleJsonString SimpleJsonBoolean

---
+++ src/test/pd/codec/json/Test_IJson.java
@@ -0,0 +1,79 @@
+package pd.codec.json;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class Test_IJson {
+
+    private static final IJsonFactory f = new SimpleJsonFactory();
+
+    @Test
+    public void test_IJson_equals_boolean() {
+        IJson json = f.createJsonBoolean(true);
+        IJson json2 = f.createJsonBoolean(true);
+        assertTrue(json != json2);
+        assertTrue(json.equals(json2));
+        assertTrue(json.hashCode() == json2.hashCode());
+    }
+
+    @Test
+    public void test_IJson_equals_float64() {
+        IJson json = f.createJsonNumber(1000099.0);
+        IJson json2 = f.createJsonNumber(1000099.0D);
+        assertTrue(json != json2);
+        assertTrue(json.equals(json2));
+        assertTrue(json.hashCode() == json2.hashCode());
+    }
+
+    @Test
+    public void test_IJson_equals_int64() {
+        IJson json = f.createJsonNumber(1000099);
+        IJson json2 = f.createJsonNumber(1000099L);
+        assertTrue(json != json2);
+        assertTrue(json.equals(json2));
+        assertTrue(json.hashCode() == json2.hashCode());
+    }
+
+    @Test
+    public void test_IJson_equals_null() {
+        IJson json = f.getJsonNull();
+        IJson json2 = f.getJsonNull();
+        assertTrue(json == json2);
+    }
+
+    @Test
+    public void test_IJson_equals_sequence() {
+        IJson json = f.createJsonArray()
+                .append(f.createJsonString("a"))
+                .append(f.createJsonNumber(2));
+        IJson json2 = f.createJsonArray()
+                .append(f.createJsonString("a"))
+                .append(f.createJsonNumber(2));
+        assertTrue(json != json2);
+        assertTrue(json.equals(json2));
+        assertTrue(json.hashCode() == json2.hashCode());
+    }
+
+    @Test
+    public void test_IJson_equals_string() {
+        IJson json = f.createJsonString("yes");
+        IJson json2 = f.createJsonString("yes");
+        assertTrue(json != json2);
+        assertTrue(json.equals(json2));
+        assertTrue(json.hashCode() == json2.hashCode());
+    }
+
+    @Test
+    public void test_IJson_equals_struct() {
+        IJson json = f.createJsonObject()
+                .set("a", f.createJsonString("1"))
+                .set("b", f.createJsonNumber(2));
+        IJson json2 = f.createJsonObject()
+                .set("a", f.createJsonString("1"))
+                .set("b", f.createJsonNumber(2));
+        assertTrue(json != json2);
+        assertTrue(json.equals(json2));
+        assertTrue(json.hashCode() == json2.hashCode());
+    }
+}
