9810e6333dc22d5d1bc0704f9012c6c72adfb0a8
tanghao <tanghao.hb@gmail.com>
2024-09-21 08:20:09 +0800

    minor refactoring

--- pd-injector/src/java/pd/injector/PrioritizedClass.java
+++ pd-injector/src/java/pd/injector/PrioritizedClass.java
@@ -4,18 +4,14 @@ import java.util.Comparator;
 
 class PrioritizedClass {
 
-    public static final Comparator<PrioritizedClass> comparator = new Comparator<PrioritizedClass>() {
-
-        @Override
-        public int compare(PrioritizedClass o1, PrioritizedClass o2) {
-            int d = o1.priority - o2.priority;
-            if (d != 0) {
-                return d;
-            }
-            return Comparator.comparing(String::toString).compare(
-                    o1.clazz.getCanonicalName(),
-                    o2.clazz.getCanonicalName());
+    public static final Comparator<PrioritizedClass> comparator = (o1, o2) -> {
+        int d = o1.priority - o2.priority;
+        if (d != 0) {
+            return d;
         }
+        return Comparator.comparing(String::toString).compare(
+                o1.clazz.getCanonicalName(),
+                o2.clazz.getCanonicalName());
     };
 
     public Class<?> clazz;
