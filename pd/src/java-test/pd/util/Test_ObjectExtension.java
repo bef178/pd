198f8ccf081250c1e9bfdd6b25c5589dee320196
tanghao <tanghao.hb@gmail.com>
2024-09-30 17:49:42 +0800

    remove jackson dep from pd

--- pd/src/java-test/pd/util/Test_ObjectExtension.java
+++ pd/src/java-test/pd/util/Test_ObjectExtension.java
@@ -1,6 +1,7 @@
 package pd.util;
 
 import org.junit.jupiter.api.Test;
+import pd.jaco.JsonMan;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -9,7 +10,7 @@ public class Test_ObjectExtension {
     @Test
     public void testConvertDouble() {
         String s = "2.0";
-        Object o = JacksonUtil.jacksonDeserialize(s, Object.class);
+        Object o = new JsonMan().deserialize(s, Object.class, ".");
         Double value = ObjectExtension.convert(o, Double.class);
         assertEquals(new Double(2), value);
     }
