b5687f1ae83be4c1735d1713161d7eca32ec0a9d
tanghao <tanghao.hb@gmail.com>
2024-10-05 09:47:03 +0800

    jaco: start path defaults to class simple name when converting

--- pd/src/java/pd/jaco/JsonMan.java
+++ pd/src/java/pd/jaco/JsonMan.java
@@ -9,6 +9,10 @@ public class JsonMan {
         return jacoMan.toJson(jaco);
     }
 
+    public <T> T deserialize(String json, Class<T> targetClass) {
+        return deserialize(json, targetClass, targetClass.getSimpleName());
+    }
+
     public <T> T deserialize(String json, Class<T> targetClass, String startPath) {
         Object jaco = jacoMan.fromJson(json);
         return jacoMan.toEntity(jaco, targetClass, startPath);
