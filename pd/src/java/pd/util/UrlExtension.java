083682d8c7c23a18c35f16ff66ce76b0f5891ca8
tanghao <tanghao.hb@gmail.com>
2025-03-18 00:26:41 +0800

    add UrlExtension.basename()

--- pd/src/java/pd/util/UrlExtension.java
+++ pd/src/java/pd/util/UrlExtension.java
@@ -4,6 +4,10 @@ import java.net.URI;
 
 public class UrlExtension {
 
+    public static String basename(String url) {
+        return PathExtension.basename(URI.create(url).getPath());
+    }
+
     public static String resolveUrl(String baseUrl, String relativePath) {
         return URI.create(baseUrl).resolve(relativePath).toString();
     }
