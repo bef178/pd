c61e330fa68856586fde907134df291460d58df9
tanghao <tanghao.hb@gmail.com>
2025-11-03 13:30:54 +0800

    move JacoGetter/Setter into JacoMan

--- pd/src/java/pd/jaco/JacoMan.java
+++ pd/src/java/pd/jaco/JacoMan.java
@@ -1,6 +1,12 @@
 package pd.jaco;
 
 import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import lombok.NonNull;
 
 /**
  * Define T as a simple java container object type:
@@ -17,24 +23,155 @@ import java.util.Arrays;
  */
 public class JacoMan {
 
-    private final JacoGetter jacoGetter = new JacoGetter();
-    private final JacoSetter jacoSetter = new JacoSetter(jacoGetter);
-
     public Object getWithPath(Object o, String path) {
         checkPath(path);
         if (o == null) {
             return null;
         }
-        return jacoGetter.get(o, Arrays.asList(path.split("/")));
+        return get(o, Arrays.asList(path.split("/")));
+    }
+
+    private Object get(@NonNull Object o, List<String> keys) {
+        for (String key : keys) {
+            o = get(o, key);
+            if (o == null) {
+                break;
+            }
+        }
+        return o;
+    }
+
+    /**
+     * throws {@link JacoException}
+     */
+    private Object get(@NonNull Object o, String key) {
+        if (o instanceof Map) {
+            Map<?, ?> m = (Map<?, ?>) o;
+            return m.getOrDefault(key, null);
+        } else if (o instanceof List) {
+            List<?> a = (List<?>) o;
+            int index;
+            try {
+                index = Integer.parseInt(key);
+            } catch (NumberFormatException e) {
+                throw JacoException.keyNotIndex(key);
+            }
+            if (index < 0) {
+                throw JacoException.negativeIndex(index);
+            } else if (index < a.size()) {
+                return a.get(index);
+            } else {
+                return null;
+            }
+        } else if (o.getClass().isArray()) {
+            Object[] a = (Object[]) o;
+            int index;
+            try {
+                index = Integer.parseInt(key);
+            } catch (NumberFormatException e) {
+                throw JacoException.keyNotIndex(key);
+            }
+            if (index < 0) {
+                throw JacoException.negativeIndex(index);
+            } else if (index < a.length) {
+                return a[index];
+            } else {
+                return null;
+            }
+        } else {
+            // XXX reflection get?
+            throw JacoException.invalidCollection(o.getClass().getSimpleName());
+        }
     }
 
     public Object setWithPath(Object o, String path, Object value) {
         checkPath(path);
         if (o == null) {
-            o = jacoSetter.createWithNextKey(path.split("/")[0]);
+            o = createWithNextKey(path.split("/")[0]);
         }
-        jacoSetter.set(o, Arrays.asList(path.split("/")), value);
+        set(o, Arrays.asList(path.split("/")), value);
         return o;
+    }
+
+    private Object set(@NonNull Object o, List<String> keys, Object value) {
+        for (int i = 0; i < keys.size() - 1; i++) {
+            String key = keys.get(i);
+            Object o1 = get(o, key);
+            if (o1 == null) {
+                String nextKey = keys.get(i + 1);
+                o1 = createWithNextKey(nextKey);
+                set(o, key, o1);
+            }
+            o = o1;
+        }
+        return set(o, keys.get(keys.size() - 1), value);
+    }
+
+    /**
+     * throws {@link JacoException}
+     */
+    private Object set(@NonNull Object o, String key, Object value) {
+        if (o instanceof Map) {
+            @SuppressWarnings("unchecked")
+            Map<Object, Object> m = (Map<Object, Object>) o;
+            return m.put(key, value);
+        } else if (o instanceof List) {
+            @SuppressWarnings("unchecked")
+            List<Object> a = (List<Object>) o;
+            int index;
+            try {
+                index = Integer.parseInt(key);
+            } catch (NumberFormatException e) {
+                throw JacoException.keyNotIndex(key);
+            }
+            if (index < 0) {
+                throw JacoException.negativeIndex(index);
+            } else if (index < a.size()) {
+                return a.set(index, value);
+            } else {
+                while (a.size() < index) {
+                    a.add(null);
+                }
+                a.add(value);
+                return null;
+            }
+        } else if (o.getClass().isArray()) {
+            int index;
+            try {
+                index = Integer.parseInt(key);
+            } catch (NumberFormatException e) {
+                throw JacoException.keyNotIndex(key);
+            }
+            Object[] a = (Object[]) o;
+            if (index < 0) {
+                throw JacoException.negativeIndex(index);
+            } else if (index < a.length) {
+                Object old = a[index];
+                a[index] = value;
+                return old;
+            } else {
+                throw JacoException.indexTooLarge(index, a.length);
+            }
+        } else {
+            // XXX reflection get?
+            throw JacoException.invalidCollection(o.getClass().getSimpleName());
+        }
+    }
+
+    private Object createWithNextKey(String nextKey) {
+        boolean prefersSequential = false;
+        try {
+            int nextIndex = Integer.parseInt(nextKey);
+            if (nextIndex >= 0 && nextIndex < 64) {
+                prefersSequential = true;
+            }
+        } catch (Exception ignored) {
+        }
+        if (prefersSequential) {
+            return new LinkedList<>();
+        } else {
+            return new LinkedHashMap<>();
+        }
     }
 
     private void checkPath(String path) {
